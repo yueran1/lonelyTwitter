d99e67c232cbcb9eed5a589775871b7d1f3896d1
Ali Sajedi Badashian <sajediba@cs.ualberta.ca>
2016-02-02 19:44:27 -0700

    Javadocs added.

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -11,6 +11,7 @@ import java.util.Date;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -18,7 +19,96 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
+/**
+ * The main activity for a small, personal Twitter app to capture, <br>notes and comments.
+ * <p>It saves the <pre>input tweets</pre> in the json files.</p>
+ * A sample code is as:<br>
+ *     <code>
+ *         for(int i=0; i<10; i++)
+ *         		for (int j=0; j<i; j++)
+ *         			doSomething();
+ *     </code>
+ * The list of im[portant activities in this class are as follows:
+ * <ul>
+ *     <li>item 1</li>
+ *     <li>item 2</li>
+ *     <li>item 3</li>
+ *     <li>item 4</li>
+ * </ul>
+ * @since 1.2.1
+ * @see LonelyTwitterActivity for more information
+ * @author Ali
+ * @version 2.3
+ * @deprecated
+ */
 public class LonelyTwitterActivity extends Activity {
+	/**
+	 * This arraylist is for keeping the tweets and their date of posting. <br>
+	 * This is blah blah.
+	 * @see #loadFromFile()
+	 */
+	public ArrayList<String> listOfItems;
+	static final String GENERAL_FILE_NAME = "fielName.json";
+
+	private int calculateTweetSize(){
+		//
+		return -1;
+	}
+
+	private String removeStopWords(String text){
+		//
+		return "";
+	}
+
+	/**
+	 * This starts the next activity which is blah blah.
+	 * @param intent This is the intent to be run immediately after hitting "start" button.
+	 */
+	private void startSecondActivity(Intent intent){
+		//
+	}
+
+	/**
+	 * This method does something!
+	 * @param s is some parameter!
+	 * @return The value that is used for some job!
+	 * @throws ...
+	 * @deprecated
+	 */
+	public String someMethod(String s){
+		return "";
+	}
+
+	public boolean evaluateOtherActivity(Intent intent){
+		int count = 0;
+		String s = "";
+		String expression1 = "", expression2 = "", expression3 = "", expression4 = "";
+
+		//This is a call to intent to do ...
+		Intent intent1 = new Intent();
+		startSecondActivity(intent1);
+		String S = someMethod(expression1 + expression2 + expression3 +
+							expression4);
+		someMethod(expression1 + expression2 + expression3 +
+				expression4);
+
+		for (int i=0; i<10; i++)
+		try{
+			int a = 1;
+			int b = 2;
+			if (a < 2) {
+				someMethod("first choice");
+			} else
+				someMethod("Second choice");
+			while (1 < 2){
+				int j = 0;
+				//
+			}
+		}
+		catch (Exception e){}
+		return true;
+	}
+
 
 	private static final String FILENAME = "file.sav";
 	private EditText bodyText;
